f356c8f09b9afb21c403982ba97c1be2e180b827
kiranchigurupati <38848808+kiranchigurupati@users.noreply.github.com>
2018-05-22 10:19:20 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -4,14 +4,14 @@ import media
 import fresh_tomatoes
 
 IT = media.Movie("IT", "horror", "it.jpg",
-                     "https://www.youtube.com/watch?v=FnCdOQsX5kc")
-CG = media.Movie("CONJURING", "horror",
-                     "con.jpg", "https://www.youtube.com/watch?v=k10ETZ41q5o")
-CT = media.Movie("CLOWNTEREGIEST", "horror",
-                     "ct.jpg", "https://www.youtube.com/watch?v=N7kXimwTUtE")
-AN = media.Movie("ANNABELLE", "horror",
-                     "an.jpg", "https://www.youtube.com/watch?v=KisPhy7T__Q")
-PA = media.Movie("PARI", "horror",
-                     "pari.jpg", "https://www.youtube.com/watch?v=PQKu78NnyvU")
+                 "https://www.youtube.com/watch?v=FnCdOQsX5kc")
+CG = media.Movie("CONJURING", "horror", "con.jpg",
+                 "https://www.youtube.com/watch?v=k10ETZ41q5o")
+CT = media.Movie("CLOWNTEREGIEST", "horror", "ct.jpg",
+                 "https://www.youtube.com/watch?v=N7kXimwTUtE")
+AN = media.Movie("ANNABELLE", "horror", "an.jpg",
+                 "https://www.youtube.com/watch?v=KisPhy7T__Q")
+PA = media.Movie("PARI", "horror", "pari.jpg",
+                 "https://www.youtube.com/watch?v=PQKu78NnyvU")
 movies = [IT, CG, CT, AN, PA]
 fresh_tomatoes.open_movies_page(movies)
